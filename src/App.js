ba4be4d31c8d5270d8721c894ee81f0cc51657db
arbaz <arbazkhan971@gmail.com>
2022-05-03 21:11:00 +0530

    added files with final changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-
+import axios from "axios";
+import {useState} from "react";
 function App() {
+
+  const [author,setauthor] = useState("Gandhi");
+  const [quote,setquote] = useState("Be the change you wish to see in the world  ");
+
+  console.log("author",author);
+  console.log("quotes",quote);
+
+  const handleClick = () =>{
+
+    axios.get("https://api.quotable.io/random").then((res)=>{
+      setauthor(res.data.author);
+      setquote(res.data.content);
+    })
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="quote-box">
+      <div className="quote-text">
+        <p> {quote} </p>
+      </div>
+      <div className="quote-author">
+        <h3> {author} </h3>
+      </div>
+
+      <div className="quote-button">
+        <button
+          onClick={handleClick}
         >
-          Learn React
-        </a>
-      </header>
+          New Quote</button>
+      </div>
     </div>
+
   );
 }
 
